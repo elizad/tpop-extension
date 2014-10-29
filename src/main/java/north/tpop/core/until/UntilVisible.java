e9973a7de3eeac3d2f58adc196aceed8407925d9
North <robyter@gmail>
2014-10-29 22:50:10 +0200

    updated UntilVisible with timeout

--- src/main/java/north/tpop/core/until/UntilVisible.java
+++ src/main/java/north/tpop/core/until/UntilVisible.java
@@ -1,5 +1,6 @@
 package north.tpop.core.until;
 
+import java.time.Duration;
 import north.tpop.core.Until;
 import north.tpop.core.Element;
 import org.openqa.selenium.WebDriver;
@@ -9,8 +10,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class UntilVisible implements Until {
 
     @Override
-    public void apply(Element element, WebDriver driver) {
-        WebDriverWait wait = new WebDriverWait(driver, 10);
+    public void apply(Element element, WebDriver driver, Duration timeout) {
+        WebDriverWait wait = new WebDriverWait(driver, timeout.getSeconds());
         wait.until(ExpectedConditions.visibilityOf(element.getWebElement()));
     }
 
