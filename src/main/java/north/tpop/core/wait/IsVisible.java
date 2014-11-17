6621d6372664dfc0f1b293961ae54fb7d9592c83
North <robyter@gmail>
2014-11-17 09:17:27 +0200

    update for WaitExecutor

---
+++ src/main/java/north/tpop/core/wait/IsVisible.java
@@ -0,0 +1,14 @@
+package north.tpop.core.wait;
+
+import north.tpop.core.Element;
+import north.tpop.core.WaitPredicate;
+import org.openqa.selenium.WebDriver;
+
+public class IsVisible implements WaitPredicate {
+
+    @Override
+    public boolean apply(Element element, WebDriver driver) {
+        return element.isDisplayed();
+    }
+
+}
